70cfce2f5910120019d35e227290c43c9f34abe9
prasannawitcher <prasannawitcher@gmail.com>
2022-01-07 17:15:18 +0530

    Customer details query change

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,10 +15,12 @@ class ApiController extends AbstractController
      */
     public function index($id='', EntityManagerInterface $em): Response
     {
-        $sql = 'SELECT ACC.*, CON.* , CV.*
+        $sql = 'SELECT CV.salutation, CV.title, CV.firstname, CV.surname, ACC.id AS accountId, CV.street, CV.houseNumber, CV.zip, CV.city, CV.countryId, CV.emailPrivate, CV.phoneHome, CV.phoneBusiness, CV.phoneMobile, ACC.balance AS accountBalance, ACC.totalDebitDue, ACC.exportedAt AS SetupDate, PV.contractInvoiceCycleInterval, PV.contractInvoiceCycleIntervalType, CRV.licensePlate
         FROM accounts AS ACC
         LEFT JOIN contracts AS CON ON CON.accountid = ACC.id
         LEFT JOIN customer_versions AS CV ON CON.customerId = CV.customerId
+        LEFT JOIN product_versions AS PV ON PV.clientId = CV.clientId
+        LEFT JOIN contract_vehicles AS CRV ON CRV.clientId = CV.clientId
         WHERE 1 ';
 
         if(!empty($id))
